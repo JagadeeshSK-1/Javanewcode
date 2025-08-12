d3f1010744a5f6321e67920636dd463de4d78abf
JagadeeshSK-1 <Kumbarjagadeesh74@gmail.com>
2025-08-12 19:20:52 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,16 @@
+class Banking
+{
+   String name;
+   int   accnumber;
+   public static void main(String[]args)
+   {
+     Banking C1=new Banking();
+     C1.name="Dinga";
+     C1.accnumber=123456;
+     Banking C2=new Banking();
+     C2.name="Jagadeesh sk";
+     C2.accnumber=7090361998l;
+     System.out.println(C1.name+" "+C1.accnumber);
+     System.out.println(C2.name+" "+C2.accnumber);
+   }
+}  
